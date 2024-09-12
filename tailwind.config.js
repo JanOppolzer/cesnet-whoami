db8b666d99edf6632d4bd30dbea341c1fec305d7
Jan Oppolzer <jan@oppolzer.cz>
2024-09-12 10:18:32 +0200

    Update for MFA

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        cesnetblue: '#0068a2',
-        freshblue: '#00a1de',
-        darkgrey: '#5a5a5a',
-        lightgrey: '#aaaaaa',
-        cesnetred: '#e60000',
-        ruby: '#a70057',
-      }
+        cesnetblue: "#0068a2",
+        freshblue: "#00a1de",
+        darkgrey: "#5a5a5a",
+        lightgrey: "#aaaaaa",
+        cesnetred: "#e60000",
+        ruby: "#a70057",
+      },
+      listStyleType: {
+        square: "square",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,9 @@ let mix = require('laravel-mix');
 
 mix
     .copy('src/*.html', 'dist/')
-    .copy('src/img/', 'dist/img/')
+    .copy('src/intercept/*.html', 'dist/intercept/')
+    .copy('src/webauthn/*.html', 'dist/webauthn/')
+    .copy('src/idp/', 'dist/idp/')
     .postCss('src/css/app.css', 'dist/css/', [
         require('tailwindcss'),
     ]);
